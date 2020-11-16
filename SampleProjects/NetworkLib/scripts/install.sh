8621618aff20aafbac63bfd2c4573e94e3b8a71e
Preston Carman <prestonc@apache.org>
2020-11-15 18:13:26 -0800

    Remove debug.

--- SampleProjects/NetworkLib/scripts/install.sh
+++ SampleProjects/NetworkLib/scripts/install.sh
@@ -2,11 +2,7 @@
 
 # if we don't have an Ethernet library already (say, in new install or for an automated test),
 # then get the custom one we want to use for testing
-echo "Inside install script"
 cd $(bundle exec arduino_library_location.rb)
-pwd
 if [ ! -d ./Ethernet ] ; then
-  echo "Before clone"
   git clone -v --depth 1 https://github.com/Arduino-CI/Ethernet.git
-  echo "Complete clone"
 fi
